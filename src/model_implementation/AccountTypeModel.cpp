f1bd9a0b545c2da493fa61619914df8fc4382717
Karen Andrea Sanabria González <168386906+KarenY93@users.noreply.github.com>
2024-12-15 16:28:57 -0500

    Update AccountTypeModel.cpp

--- src/model_implementation/AccountTypeModel.cpp
+++ src/model_implementation/AccountTypeModel.cpp
@@ -1,1 +1,23 @@
+#include "../../include/models/AccountTypeModel.hpp"
+#include "../../include/models/AccountModel.hpp"
+#include <iostream>
 
+AccountTypeModel::AccountTypeModel(std::string owner, std::string accountType, int accountNumber, std::string status, std::string creationDate, float balance, std::string type, float i, float l, float m)
+    : AccountModel(owner, accountType, accountNumber, status, creationDate, balance), interest(i), limit(l), maintenanceCost(m) {}
+    
+float AccountTypeModel::GetInterest() { return interest; }
+
+float AccountTypeModel::GetLimit() { return limit; }
+
+float AccountTypeModel::GetMaintenanceCost() { return maintenanceCost; }
+
+std::string AccountTypeModel::ToString() {
+    return AccountModel::ToString() + std::to_string(interest) + "," + std::to_string(limit) + "," + std::to_string(maintenanceCost) + "\n";
+}
+
+void AccountTypeModel::ShowInfo() {
+    AccountModel::ShowInfo();
+    std::cout << "Interes: " << interest << std::endl;
+    std::cout << "Limite: " << limit << std::endl;
+    std::cout << "Cuota de mantenimiento: " << maintenanceCost << std::endl;
+}
