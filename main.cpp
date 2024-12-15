85b1b0e85ebeab14c861a432ad8322387bb1fdc1
Karen Andrea Sanabria González <168386906+KarenY93@users.noreply.github.com>
2024-12-15 16:31:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,12 @@
-#include "include/models/ClientModel.hpp"
-#include "include/service/ClientService.hpp"
-#include "include/models/AccountModel.hpp"
-#include "include/service/AccountService.hpp"
+#include "include/view/AccountView.hpp"
 
 #include <iostream>
 #include <vector>
 
-int main() { 
-  ClientService service;
-  ClientModel client(123,"sebastian",15);
-  ClientModel client1(124,"karen",18);
-  service.Add(client);
-  service.Add(client1);
+int main() {
 
+    AccountView view;
+    view.ShowMenu();
+  
   return 0;
 }
