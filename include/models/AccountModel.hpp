e1dd2e653d3f3f349ba8dd7168f8540aeff755ae
Karen Andrea Sanabria González <168386906+KarenY93@users.noreply.github.com>
2024-12-15 16:24:29 -0500

    Update AccountModel.hpp

--- include/models/AccountModel.hpp
+++ include/models/AccountModel.hpp
@@ -1,28 +1,35 @@
 #pragma once
 #include <string>
+#include <iostream>
 
-class AccountModel {
+
+class AccountModel{
 
 private:
-  float balance;
   std::string owner;
+  std::string type;
   int accountNumber;
   std::string status;
   std::string creationDate;
+  float balance;
 
 public:
-  AccountModel(std::string owner, int accountNumber, std::string status, std::string creationDate, float balance);
-  float GetBalance();
+  AccountModel(std::string owner, std::string type, int accountNumber, std::string status, std::string creationDate, float balance);
   std::string GetOwner();
+  std::string GetType();
   std::string GetAccountNumber();
   std::string GetStatus();
   std::string GetCreationDate();
-  void SetBalance(float);
+  float GetBalance();
   void SetOwner(std::string);
+  void SetType(std::string);
   void SetAccountNumber(int);
   void SetStatus(std::string);
   void SetCreationDate(std::string);
+  void SetBalance(float);
   std::string ToString();
+  void ShowInfo();
+  
 
 };
 
