231db33f91ff437511b7572ef73f0f56154168b3
Karen Andrea Sanabria González <168386906+KarenY93@users.noreply.github.com>
2024-12-15 16:26:19 -0500

    Update AccountTypeService.hpp

--- include/service/AccountTypeService.hpp
+++ include/service/AccountTypeService.hpp
@@ -1,4 +1,14 @@
 #pragma once
+#include "AccountService.hpp"
 #include <iostream>
 #include <string>
 
+class AccountTypeService : public AccountService {
+    public:
+        AccountTypeService();
+        std::string filename1 = "db/accounts_savings.csv";
+        std::string filename2 = "db/accounts_current.csv";
+        void ProcessAccounts(int);
+        void Read(int numberAccount);
+        void Read2(int numberAccount);
+};
